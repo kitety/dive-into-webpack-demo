c07248921c661ea772d631fde30aefd00ac8fbc0
liuzhengkun <liuzhengkun@sobey.com>
2018-12-18 19:28:58 +0800

    plugins属性

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
     mode: 'development',
     // 入口
@@ -12,15 +14,32 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                // use: ['style-loader', 'css-loader']
-                // 或者
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader'
-                    }
-                ]
+                use: ExtractTextPlugin.extract({
+                    fallback:'style-loader',
+                    // 转换.css 文件需要的loader
+                    use: 'css-loader'
+                })
             }
+            //  之前的cssloader
+            // {
+            //     test: /\.css$/,
+            //     // use: ['style-loader', 'css-loader']
+            //     // 或者
+            //     use: [
+            //         'style-loader',
+            //         {
+            //             loader: 'css-loader'
+            //         }
+            //     ]
+            // }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            // 从.js文件中提取出来的.css的文件的名称
+            // css 需要另外引入
+            // filename:'[name]_[hash:4].css'
+            filename:'[name].css'
+        })
+    ]
 }
