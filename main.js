8273143f6d28498d4a3fe876cf1bee3593ffde7e
kitety <aizaizuori@foxmail.com>
2018-12-19 23:51:23 +0800

    新的环境搭建

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-require('./main.css')
-// 引入css的时候直接进行cssload
-// require('style-loader!css-loader!./main.css')
-
-// show函数
-const show = require('./show.js')
-show('Webpack')
+console.log(111111)
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,39 @@
 const path = require('path')
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    // 入口
-    // 修改入口的对应的依赖才会热更新，而对应index.html不会触发，因为index.html脱离了js的模块存在
-    entry: './main.js',
-    // 出口
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname + '/dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    // 转换.css 文件需要的loader
-                    use: 'css-loader'
-                })
-            }
-            //  之前的cssloader
-            // {
-            //     test: /\.css$/,
-            //     // use: ['style-loader', 'css-loader']
-            //     // 或者
-            //     use: [
-            //         'style-loader',
-            //         {
-            //             loader: 'css-loader'
-            //         }
-            //     ]
-            // }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin({
-            // 从.js文件中提取出来的.css的文件的名称
-            // css 需要另外引入
-            // filename:'[name]_[hash:4].css'
-            filename: '[name].css'
-        })
+  mode: 'development',
+  // 入口
+  // 修改入口的对应的依赖才会热更新，而对应index.html不会触发，因为index.html脱离了js的模块存在
+  entry: './main.js',
+  // 出口
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname + '/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+      //  之前的cssloader
+      // {
+      //     test: /\.css$/,
+      //     // use: ['style-loader', 'css-loader']
+      //     // 或者
+      //     use: [
+      //         'style-loader',
+      //         {
+      //             loader: 'css-loader'
+      //         }
+      //     ]
+      // }
     ]
+  }
 }
